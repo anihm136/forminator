d401488d8c3626728ec2c302b93752ecdce563d0
Anirudh H M <anihm136@gmail.com>
2021-06-09 02:31:07 +0530

    Create form handler

---
+++ backend/src/handlers/create.ts
@@ -0,0 +1,56 @@
+import { Request, Response, NextFunction } from 'express';
+import { CustomResponse } from '../types/response'
+import { Form, FormType } from '../models/form';
+import { MCQ, SAQ } from '../models/question';
+import { validator } from '../schemas';
+
+const runtimeValidateForm = (form: FormType) => {
+    for (const [idx, question] of form.elements.entries()) {
+        if (question.type == 2 && question.correct >= question.choices.length) {
+            return false;
+        }
+        switch (question.type) {
+            case 1:
+                form.elements[idx] = new SAQ(question)
+            case 2:
+                form.elements[idx] = new MCQ(question)
+        }
+    }
+
+    return true;
+};
+
+const createForm = async (req: Request, res: CustomResponse, next: NextFunction) => {
+    const validateForm = validator.getSchema('form');
+
+    if (!validateForm) {
+        res.status(500).send({ message: 'Unable to validate form' });
+        return;
+    }
+
+    let response = {
+        code: 200,
+        message: 'Form created',
+        form_id: ''
+    };
+
+    const form = new Form(req.body);
+    if (!validateForm(req.body) || !runtimeValidateForm(form)) {
+        response.code = 400;
+        response.message = 'Form invalid';
+        res.custom_data = response
+        next()
+    } else {
+        form.save((err: any, self: FormType): void => {
+            if (err) {
+                response.code = 400;
+                response.message = 'Form not created';
+            }
+            response.form_id = self._id.toString()
+            res.custom_data = response
+            next()
+        });
+    }
+};
+
+export default createForm;
