882065c7f70dea5e79f61c5ce4f841a9df6d013d
Anirudh H M <anihm136@gmail.com>
2021-06-14 01:43:24 +0530

    Refactor handler and helpers

---
+++ src/handlers/create/index.ts
@@ -0,0 +1,54 @@
+import { Request, NextFunction } from 'express'
+import { MCQ, SAQ } from '../../models/question'
+import { CustomResponse } from '../../types/response'
+import { Form, FormType } from '../../models/form'
+import runtimeValidateForm from './runtimeValidate'
+import schemaValidateForm from './schemaValidate'
+
+const createForm = async (req: Request, res: CustomResponse, next: NextFunction): Promise<void> => {
+  const response = {
+    code: 201,
+    message: 'Form created',
+    form_id: ''
+  }
+
+  if (!schemaValidateForm(req.body)) {
+    response.code = 400
+    response.message = 'Form invalid'
+    res.custom_data = response
+    next()
+  }
+
+  const form = new Form(req.body)
+  if (!runtimeValidateForm(form)) {
+    response.code = 400
+    response.message = 'Form invalid'
+    res.custom_data = response
+    next()
+  }
+
+  for (const [idx, question] of form.elements.entries()) {
+    switch (question.type) {
+      case 1:
+        form.elements[idx] = new SAQ(question)
+        break
+      case 2:
+        form.elements[idx] = new MCQ(question)
+        break
+    }
+  }
+
+  form.save((err: any, self: FormType): void => {
+    if (err) {
+      response.code = 400
+      response.message = 'Form not created'
+    } else {
+      response.form_id = self._id.toString()
+    }
+    res.custom_data = response
+    next()
+  })
+}
+
+export default createForm
+export { createForm, runtimeValidateForm }
---
+++ src/handlers/create/runtimeValidate.ts
@@ -0,0 +1,15 @@
+import { FormType } from '../../models/form'
+
+function runtimeValidateForm(form: FormType): boolean {
+  for (const question of form.elements) {
+    if ('choices' in question) {
+      if (question.correct >= question.choices.length) {
+        return false
+      }
+    }
+  }
+
+  return true
+}
+
+export default runtimeValidateForm
